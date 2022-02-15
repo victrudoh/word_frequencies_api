8e089e1401e5c7a3e3978999bcf14f8266b9e473
Edikan <victrudoh@gmail.com>
2022-02-15 15:58:10 +0100

    minor changes

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.set("view engine", "ejs"); // template engine
-app.set("views", path.join(__dirname, "/views")); // setting views directory
-app.use(express.static(path.join(__dirname, "/public"))); // static files directory
-
 const indexRouter = require("./routes/index.routes");
 
 app.use("/", indexRouter);
